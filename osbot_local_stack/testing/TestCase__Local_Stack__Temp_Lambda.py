792b541232a09b6ffdd43ea1cdb1662b5181fca0
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-14 15:34:08 +0100

    Added first implementation of TestCase__Local_Stack__Temp_Lambda

--- osbot_local_stack/testing/TestCase__Local_Stack__Temp_Lambda.py
+++ osbot_local_stack/testing/TestCase__Local_Stack__Temp_Lambda.py
@@ -1,11 +1,44 @@
+from osbot_utils.context_managers.print_duration                 import print_duration
+from osbot_utils.testing.Temp_Env_Vars                           import Temp_Env_Vars
+from osbot_utils.utils.Env                                       import set_env
+from osbot_aws.deploy.Deploy_Lambda                              import Deploy_Lambda
+from osbot_local_stack.aws.lambdas.dev.temp_lambda               import run
 from osbot_local_stack.testing.TestCase__Local_Stack__Temp_Bucket import TestCase__Local_Stack__Temp_Bucket
+
+DEPLOY_LAMBDA__UPDATE_WAIT_TIME = 1.0
 
 class TestCase__Local_Stack__Temp_Lambda(TestCase__Local_Stack__Temp_Bucket):
 
     @classmethod
     def setUpClass(cls):
-        super().setUpClass()
+        super().setUpClass()                                                                                    # call the base class setUpClass(),  which will create the temp S3 Bucket (in Local Stack)
+        cls.temp_env_vars   = Temp_Env_Vars(env_vars={'OSBOT_LAMBDA_S3_BUCKET': cls.s3_bucket}).set_vars()      # temporarily set the OSBOT_LAMBDA_S3_BUCKET to the temp S3 Bucket
+        cls.deploy_lambda   = Deploy_Lambda(run)                                                                # create a Deploy_Lambda instance with the lambda handler from osbot_local_stack.aws.lambdas.dev.temp_lambda
+        cls.lambda_function = cls.deploy_lambda.lambda_function()                                               # helper variable to reference the lambda function
+        cls.create_temp_lambda()                                                                                # create the lambda function
 
     @classmethod
     def tearDownClass(cls):
+        cls.delete_temp_lambda()
         super().tearDownClass()
+        cls.temp_env_vars.restore_vars()
+
+    @classmethod
+    def create_temp_lambda(cls):
+        with print_duration():
+            update_status = cls.deploy_lambda.update()
+            if update_status == "Pending":
+                wait_time     = DEPLOY_LAMBDA__UPDATE_WAIT_TIME
+                update_status = cls.deploy_lambda.lambda_function().wait_for_function_update_to_complete(wait_time=wait_time)       # in GH actions and locally it can take up to 10 seconds to update the lambda on the first run (I think due to the time it takes to download the docker image)
+            assert update_status == 'Successful'
+            assert cls.deploy_lambda.exists() is True
+
+    @classmethod
+    def delete_temp_lambda(cls):
+        assert cls.deploy_lambda.delete() is True
+
+    def invoke(self, params=None):
+        return self.lambda_function.invoke(params)
+
+    def invoke__return_logs(self, params=None):
+        return self.lambda_function.invoke_return_logs(params)
--- tests/integration/testing/test_TestCase__Local_Stack__Temp_Lambda.py
+++ tests/integration/testing/test_TestCase__Local_Stack__Temp_Lambda.py
@@ -1,39 +1,66 @@
-from osbot_aws.aws.s3.S3                                            import S3
-from osbot_utils.utils.Env                                          import set_env
-from osbot_local_stack.local_stack.Local_Stack__Internal            import DEFAULT__LOCAL_STACK__TARGET_SERVER
-from osbot_aws.deploy.Deploy_Lambda                                 import Deploy_Lambda
-from osbot_local_stack.aws.lambdas.dev.hello_world                  import run
+from osbot_utils.utils.Misc import list_set
+
+from osbot_utils.utils.Objects import dict_to_obj
+
+from osbot_local_stack.aws.lambdas.dev.temp_lambda                  import temp_lambda_return_message
+from osbot_utils.utils.Env                                          import get_env
 from osbot_local_stack.testing.TestCase__Local_Stack__Temp_Lambda   import TestCase__Local_Stack__Temp_Lambda
-from osbot_aws.aws.lambda_.Lambda                                   import Lambda
 
 
 class test_TestCase__Local_Stack__Temp_Lambda(TestCase__Local_Stack__Temp_Lambda):
 
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        assert get_env('OSBOT_LAMBDA_S3_BUCKET') is None
+        assert cls.lambda_function.exists()      is False
+
     def test__setUpClass(self):
         lambda_name = 'an-test-lambda'
         #with Lambda() as _:
-        set_env('OSBOT_LAMBDA_S3_BUCKET', self.s3_bucket)
+        assert get_env('OSBOT_LAMBDA_S3_BUCKET') == self.s3_bucket
+        assert self.lambda_function.exists()     is True
 
-    def test_create__temp_lambda(self):
-        bucket_name = self.s3_bucket
-        with S3() as _:
-            set_env('OSBOT_LAMBDA_S3_BUCKET', bucket_name)
-            assert _.bucket_exists(bucket_name) is True
+    def test_invoke(self):
+        assert self.lambda_function.invoke() == temp_lambda_return_message
 
-        deploy_lambda = Deploy_Lambda(handler=run)
-        assert deploy_lambda.osbot_setup.s3.client().meta.endpoint_url == DEFAULT__LOCAL_STACK__TARGET_SERVER
+    def test_invoke__return_logs(self):
+        from osbot_utils.utils.Dev import pprint
+        result = dict_to_obj(self.lambda_function.invoke_return_logs())
+        assert list_set(result)    == ['execution_logs', 'name', 'request_id', 'return_value', 'status']
+        assert result.name         == 'osbot_local_stack_aws_lambdas_dev_temp_lambda'
+        assert result.return_value == temp_lambda_return_message
 
-        update_status = deploy_lambda.update()
-
-        if update_status == "Pending":
-            update_status = deploy_lambda.lambda_function().wait_for_function_update_to_complete(wait_time=0.5)  # in GH actions it can take up to 10 seconds to update the lambda
-        assert update_status  == 'Successful'
-
-        with Lambda() as _:
-           assert 'osbot_local_stack_aws_lambdas_dev_hello_world' in _.functions_names()
-
-        with S3() as _:
-             assert 'lambdas/osbot_local_stack.aws.lambdas.dev.hello_world.zip'  in _.find_files(bucket_name)
-
-        assert deploy_lambda.invoke() == 'Hello "World"'
-        assert deploy_lambda.invoke(dict(name='OSBot')) == 'Hello "OSBot"'
+    # def test_create__temp_lambda(self):
+    #     bucket_name = self.s3_bucket
+    #     #pprint(aws_config.region_name())
+    #     with S3() as _:
+    #         set_env('OSBOT_LAMBDA_S3_BUCKET', bucket_name)
+    #         assert _.bucket_exists(bucket_name) is True
+    #
+    #     deploy_lambda = Deploy_Lambda(handler=run)
+    #     assert deploy_lambda.osbot_setup.s3.client().meta.endpoint_url == DEFAULT__LOCAL_STACK__TARGET_SERVER
+    #
+    #     update_status = deploy_lambda.update()
+    #
+    #     if update_status == "Pending":
+    #         update_status = deploy_lambda.lambda_function().wait_for_function_update_to_complete(wait_time=0.5)  # in GH actions it can take up to 10 seconds to update the lambda
+    #     assert update_status  == 'Successful'
+    #
+    #     with Lambda() as _:
+    #        assert 'osbot_local_stack_aws_lambdas_dev_hello_world' in _.functions_names()
+    #
+    #     with S3() as _:
+    #          assert 'lambdas/osbot_local_stack.aws.lambdas.dev.hello_world.zip'  in _.find_files(bucket_name)
+    #
+    #     assert deploy_lambda.invoke() == 'Hello "World"'
+    #     assert deploy_lambda.invoke(dict(name='OSBot')) == 'Hello "OSBot"'
+    #
+    # def test_execute_lambda(self):
+    #     lambda_name = "osbot_local_stack_aws_lambdas_dev_hello_world"
+    #     region_name = 'ap-southeast-1'
+    #     aws_config.set_aws_session_region_name(region_name)
+    #     with Lambda(lambda_name) as _:
+    #         assert _.exists() is True
+    #         result = _.invoke_return_logs(dict(name='abc'))
+    #         pprint(result)
